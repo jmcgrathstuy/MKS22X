7904757ec9655298888200aab56213d64bc08526
jmcgrathstuy <jmcgrath@stuy.edu>
2018-02-10 03:24:16 -0500

    Classwork, added constructor, toString

---
+++ 02NQueens/QueenBoard.java
@@ -0,0 +1,34 @@
+public class QueenBoard{
+
+    private int[][] board;
+    
+    public QueenBoard(int size){
+	board = new int[size][size];
+	for( int stepperRow = 0; stepperRow < size; stepperRow++){
+	    for( int stepperCol = 0; stepperCol < size; stepperCol++){
+		board[stepperRow][stepperCol] = 0;
+	    }
+	}
+    }
+
+    public String toString(){
+	String str = "";
+	for( int stepperRow = 0; stepperRow < board.length; stepperRow++){
+	    for(int stepperCol = 0; stepperCol < board.length; stepperCol++){
+		if( board[stepperRow][stepperCol] == -1){
+		    str += "Q";
+		}else{
+		    str += board[stepperRow][stepperCol];
+		}
+	    }
+	    str += "\n";
+	}
+	return str;
+    }
+
+    private void /*boolean*/ addQueen(int r, int c){}
+    private void /*boolean*/ removeQueen(int r, int c){}
+
+
+
+}
