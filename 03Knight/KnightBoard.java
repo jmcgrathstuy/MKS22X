1745400fd66ad3f0d6f394bf674917dddd16ef27
jmcgrathstuy <jmcgrath@stuy.edu>
2018-02-14 03:25:40 -0500

    Wrote toString

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -4,13 +4,28 @@ public class KnightBoard{
     
     public KnightBoard(int startingRows, int startingCols){
 	board = new int[startingRows][startingCols];
-	for(int iR = 0; int iR < startingRows; iR++){
-	    for(int iC = 0; int iC < startingCols; iC++){
+	for(int iR = 0;  iR < startingRows; iR++){
+	    for(int iC = 0;  iC < startingCols; iC++){
 		board[iR][iC] = 0;
 	    }
 	}
 	
     }
+
+    public String toString(){
+	String str = "";
+	for(int iR = 0; iR < board.length; iR++){
+	    for(int iC = 0;  iC < board[iR].length; iC++){
+		if(board[iR][iC] < 10){
+		    str += " " + board[iR][iC] + " ";
+		}else{
+		    str += board[iR][iC] + " ";
+		}
+	    }
+	    str += "\n";
+	    }
+	return str;
+	}
 
 
 
