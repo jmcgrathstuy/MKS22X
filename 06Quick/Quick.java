e90b4858b9fcddba377e191ed1beb115094d27dd
jmcgrathstuy <jmcgrath@stuy.edu>
2018-03-17 00:07:19 -0400

    period just ended no warning bell. you figure it out, me

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -15,7 +15,24 @@ public class Quick{
 
     public void partition(int[] ary, int lo, int hi){
 	int p = math.Random(lo, hi);
+	swap( ary, lo, p);
+	int i = p + 1;
+	whlie( i <= hi){
+	    if( ary[i] == ary[p]){
+		i++;
 
+
+
+
+
+    }
+
+    public void swap(int[] ary, int a, int b){
+	int at = ary[a];
+	ary[a] = b;
+	ary[b] = at;
+    }
+	
 	//also later do the recursion inside of quicksort/select
 	
 
