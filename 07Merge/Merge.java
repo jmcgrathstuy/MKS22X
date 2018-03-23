f64bdb3ffbc8dea797b1a72f80ca7430f58f7d72
jmcgrathstuy <jmcgrath@stuy.edu>
2018-03-23 03:28:58 -0400

    Converted merge to work with the other methods. Doesn't actually merge successfully though. Seems longer than it probably should be

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,36 +1,51 @@
 public class Merge{
 
-    /*public static int[] merge( int[] a, int[] b){
-        /*int[] tempA = new int[(eA - sA) + 1];
-        int[] tempB = new int[(eB - sB) + 1];
-        for( int j = (eA - sA +) 1, int k = (eB - sB +) 1; ; j++, k++){
+    public static void merge( int[] data, int[] temp, int aStart, int aEnd, int bStart, int bEnd){
+        /*for( int j = (eA - sA +) 1, int k = (eB - sB +) 1; ; j++, k++){
             tempA[j] = data[j];
             tempB[k] = data[k];
-        }
-        int[] tempData = new int[(eB - sA) + 1];
-        int i = 0;
-        int j = 0;
-        int[] dataToo = new int[a.length + b.length];
-        for(int k = 0; /*i < a.length || j < b.length k < dataToo.length; k++){
-            if( i == a.length){
-                while( k < dataToo.length){
-                    data[k] = b[j];
-                    j++;
+	    }*/
+        //int[] tempData = new int[(eB - sA) + 1];
+        //int i = 0;  replace i with aStart
+        //int j = 0;  replace j with bStart
+        //int[] dataToo = new int[a.length + b.length];
+        for(int k = 0; (aStart <= aEnd || bStart <= bEnd) &&  k < temp.length; k++){
+            if( aStart == aEnd){
+                while( k < temp.length){
+                    temp[k] = data[bStart];
+                    bStart++;
                     k++;
                 }
-                return dataToo;
-            }else if( j == b.length){
-                while( k < dataToo.length){
-                data[k] = b[i];
-                i++;
+                return;
+	    }
+            if( bStart == bEnd){
+                while( k < temp.length){
+                temp[k] = data[aStart];
+                aStart++;
                 k++;
                 }
-            return dataToo;
-	    }else if(){
+		return;
 	    }
+	    if(data[aStart] == data[bStart]){
+		temp[k] = data[aStart];
+		temp[k + 1] = data[bStart];
+		aStart++;
+		bStart++;
+		k+= 2;
+	    }
+	    if(data[aStart] < data[bStart]){
+		temp[k] = data[aStart];
+		aStart++;
+		k++;
+	    }
+	    if(data[aStart] > data[bStart]){
+		temp[k] = data[bStart];
+		bStart++;
+		k++;
+	    }
+	    
 	}
-
-    }*/
+    }
 
     public static void mergeSort(int[] data){
     }
@@ -51,15 +66,22 @@ public class Merge{
 
 
 
-    public static void merge(int[] data, int[]temp, int  lo, int  mid, int tSt, int  hi){
+    /* public static void merge(int[] data, int[]temp, int  lo, int  mid, int tSt, int  hi){
 	while( lo < data.length){
 
 	}
-    }
+	}*/
                
 
     public static void main(String[] args){
 	System.out.println( 5/2);
+	int[] a = {2, 5, 3, 8, 5, 6, 3};
+	int[] b = {2, 5, 3, 8, 5, 6, 3};
+	merge( a, b, 0, 4, 5, 6);
+	for( int i = 0; i < b.length; i++){
+	    
+	System.out.println(b[i]);
+	}
     }
 
 
