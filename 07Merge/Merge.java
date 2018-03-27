5fa641b590e817209f428e56dfcc9befe80e7c7f
jmcgrathstuy <jmcgrath@stuy.edu>
2018-03-26 21:39:22 -0400

    Cleaned up the code a lot, got rid of a lot of unnecessary old code. Mergesort (more specifically mSort) still doesn't sort properly and I'm struggling to figure out why.

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,18 +1,8 @@
 public class Merge{
 
     public static void merge( int[] data, int[] temp, int aStart, int aEnd, int bStart, int bEnd){
-        /*for( int j = (eA - sA +) 1, int k = (eB - sB +) 1; ; j++, k++){
-            tempA[j] = data[j];
-            tempB[k] = data[k];
-	    }*/
-        //int[] tempData = new int[(eB - sA) + 1];
-        //int i = 0;  replace i with aStart
-        //int j = 0;  replace j with bStart
-        //int[] dataToo = new int[a.length + b.length];
         for(int k = aStart; (aStart <= aEnd || bStart <= bEnd) &&  k < bEnd;){
-	    //System.out.println( "k = " + k + "\naStart = " + aStart + "\nbStart = " + bStart);
             if( aStart == aEnd){
-		//System.out.println(" if #1");
 		temp[k] = data[aStart];
 		aStart++;
 		k++;
@@ -24,7 +14,6 @@ public class Merge{
                 return;
 	    }
             if( bStart == bEnd){
-		//System.out.println(" if #2");
 		temp[k] = data[bStart];
 		bStart++;
 		k++;
@@ -36,7 +25,6 @@ public class Merge{
 		return;
 	    }
 	    if(data[aStart] == data[bStart]){
-		//System.out.println(" if #3");
 		temp[k] = data[aStart];
 		temp[k + 1] = data[bStart];
 		aStart++;
@@ -58,16 +46,22 @@ public class Merge{
     }
 
     public static void mergeSort(int[] data){
+	//if( data.length <= 1){
+	//	return;
+	//}
 	int[] temp = new int[data.length];
 	for( int i = 0; i < data.length; i++){
 	    temp[i] = data[i];
 	}
 	mSort(data, temp, 0, data.length - 1);
     }
+	
+	
+	
     private static void mSort(int[] data, int[] temp, int lo, int hi){
-	if( lo >= hi){
-	    return;
-	}
+	if( lo >= hi ){
+		return;
+	}else{
 	//for( int i = 0; i < data.length; i++){
 	//    temp[i] = data[i];
 	//}
@@ -75,6 +69,7 @@ public class Merge{
 	mSort( temp, data, lo, mid);
 	mSort( temp, data, mid + 1, hi);
 	merge( temp, data, lo, mid, mid + 1, hi);
+	}
     }
 	
 
@@ -90,17 +85,19 @@ public class Merge{
 
     public static void main(String[] args){
 	//System.out.println( 5/2);
-	int[] a = {5, 7, 9, 837, 1, 6, 14, 999};
-    int[] b = {5, 7, 9, 837, 1, 6, 14, 999};
-	merge( a, b, 0, 3, 4, 7);
+	int[] a = {5, 7, 1, 2, 1, 6, 14, 999};
+    int[] b = {5, 7, 1, 2, 1, 6, 14, 999};
+	merge( a, b, 0, 1, 2, 3);
 	for( int i = 0; i < b.length; i++){
 	    
 	//System.out.println(b[i]);
 	}
 
-	int[] c = {1, 1, 1, 1, 2, 1, 1, 1, 1, 1};
+	//int[] c = {1, 1, 1, 1, 2, 1, 1, 1, 1, 1};
+	int[]c = {3, 1, 2};
+	//mSort(c, 
 	mergeSort(c);
-	for( int i = 0; i < b.length; i++){
+	for( int i = 0; i < c.length; i++){
 	    
 	System.out.println(c[i]);
 	}
