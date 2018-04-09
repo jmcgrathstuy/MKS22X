58bb9eada4100e67a8752216ab4643e26cdc0993
jmcgrathstuy <33815496+jmcgrathstuy@users.noreply.github.com>
2018-04-09 10:10:17 -0400

    Wrote new add() method and redid Node to work with Integers

--- 08LinkedList/MyLinkedList.java
+++ 08LinkedList/MyLinkedList.java
@@ -31,7 +31,7 @@ public class MyLinkedList{
     }
     //
     public boolean add( Integer value){
-	Node newNode = new Node( value.intValue());
+	Node newNode = new Node( value);
 	if( size() != 0){
 	    last.setNext(newNode);
 	}
@@ -46,7 +46,15 @@ public class MyLinkedList{
 	}
 	cur.setData( value.intValue());
     }
-
+    public void add( int index, Integer value){
+        Node prev = getNode(index - 1)
+	Node newNode = new Node(value);
+	newNode.setNext(prev.getNext());
+	newNode.setPrev(prev);
+	prev.setNext(newNode);
+	newNode.getNext().setPrev(newNode);
+    }
+	
 
     
     public int get(int index){
@@ -57,6 +65,15 @@ public class MyLinkedList{
 	}
 	return cur.getData();
     }
+    private Node GetNode( int index){
+	    Node cur = first;
+	    for( int i = 0; i < index; i++){
+	        cur = cur. getNext();
+	    }
+	    return cur;
+    }
+		
+
 	
     public String toString(){
 	String str = "[";
@@ -87,18 +104,18 @@ public class MyLinkedList{
 
 
     private class Node{
-	int data;
+	Integer data;
         Node prev;
 	Node next;
 
-	Node(int dataIn){
+	Node(Integer dataIn){
 	    data = dataIn;
 	}
 
 	int getData(){
 	    return data;
 	}
-	void setData(int dataIn){
+	void setData(Integer dataIn){
 	    data = dataIn;
 	}
 	Node getNext(){
