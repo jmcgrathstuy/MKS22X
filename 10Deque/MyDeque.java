ca032eb818cd687e9b32db05322e29dec42e8412
jmcgrathstuy <jmcgrath@stuy.edu>
2018-04-18 15:33:09 -0400

    09 + 10 classwork additions

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class MyDeque{ //implements Deque<String>{
+    int size;
+
+    public MyDeque(){
+	size = 10;
+    }
+
+    public int size(){
+	return size;
+    }
+
+    public MyDeque(int initialCapacity){
+	size = initialCapacity;
+    }
+
+
+    public void AddFirst(String str){
+    }
+    
+
+
+
+
+
+
+}
